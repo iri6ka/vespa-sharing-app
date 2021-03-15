be1048cc3e2a3c7391680c61a5ee55385b24042c
iri6ka <ikonnova@gmail.com>
2021-03-15 16:44:29 +1100

    set up bootstrap colors for alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+    def select_alert_subclass(input)
+        case input
+        when 'notice'
+            return 'info'
+        when 'success'
+            return 'success'
+        when 'error'
+            return 'danger'
+        when 'alert'
+            return 'warning'
+        else
+            return 'warning'
+        end
+    end
 end
