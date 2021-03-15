25b92f15c658ec7152e7847319d05210807eb0a4
iri6ka <ikonnova@gmail.com>
2021-03-15 13:43:49 +1100

    set up Pages controller and User model with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/home'
   root 'home#page'
   resources :listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
